a69199e34809732bec1a2f37152652fa385214c9
Vitor da Costa Silva <vitordacostasilva@Vitors-MBP.lan>
2024-01-16 20:05:45 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.32.1"
     }
+  }
+  backend "s3" {
+    bucket = "comunidadedevops-vitor-csilva"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
