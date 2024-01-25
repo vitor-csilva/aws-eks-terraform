bf3cea7f3f541de5fde887f0b78037d3f86e8307
Vitor da Costa Silva <vitordacostasilva@Vitors-MBP.lan>
2024-01-24 21:45:41 -0300

    fix: terraform fmt

--- modules/network/private.tf
+++ modules/network/private.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "eks_subnet_private_1a" {
   availability_zone = "${data.aws_region.current.name}a"
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                              = "${var.project_name}-priv-subnet-1a",
       "kubernetes.io/role/internal-elb" = 1 #Adicionando a tag para a subnet conforme documentacao da AWS
@@ -18,10 +18,20 @@ resource "aws_subnet" "eks_subnet_private_1b" {
   availability_zone = "${data.aws_region.current.name}b"
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                              = "${var.project_name}-priv-subnet-1b",
       "kubernetes.io/role/internal-elb" = 1 #Adicionando a tag para a subnet conforme documentacao da AWS
     }
   )
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_priv_1a" {
+  subnet_id      = aws_subnet.eks_subnet_private_1a.id
+  route_table_id = aws_route_table.eks_private_route_table_1a.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_priv_1b" {
+  subnet_id      = aws_subnet.eks_subnet_private_1b.id
+  route_table_id = aws_route_table.eks_private_route_table_1b.id
 }
